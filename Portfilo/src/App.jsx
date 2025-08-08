0f2a03503a24ae9fdabe3d2d07a84e044e17f593
MohamedNagy <ms8912568@gmail.com>
2025-08-08 23:28:22 +0300

    Frist commit

---
+++ Portfilo/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import Navbar from './Pages/Navbar';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Contact from './Pages/Contact/Contact';
+import Footer from './Pages/Footer/Footer';
+import Skills from './Pages/Skiles/Skills';
+import Projects from './Pages/Projects';
+
+export default function App() {
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true });
+  }, []);
+
+  return (
+    <div className="bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100 min-h-screen transition-colors duration-300">
+      <Navbar />
+      <main>
+        <Home />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+      </main>
+    </div>
+  );
+}
